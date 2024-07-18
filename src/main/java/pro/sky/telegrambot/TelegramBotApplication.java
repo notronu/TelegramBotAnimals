1bcbc13bd1df44fb84ef4eb1b1df8e0871788cc9
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2024-07-18 18:58:47 +0300

    empty

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TelegramBotApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
